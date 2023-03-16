99a64bed6d3139afa11220e3c2fc42e62c93dabe
Leandro Siqueira <le_kts12@hotmail.com>
2023-03-15 23:33:40 -0300

    feat: (Aprendendo sobre animações com reanimated)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Pressable, Text, Button } from 'react-native';
+import { styles } from './styles';
+import Animated, { 
+  useSharedValue, 
+  useAnimatedStyle,
+  withTiming,
+  Easing,
+  withSequence,
+  interpolateColor,
+  withSpring
+} from 'react-native-reanimated';
+
+/**
+ * useAnimatedStyles
+ * useSharedValue
+ * Animated.View
+ */
+
+const PressableAnimated = Animated.createAnimatedComponent(Pressable);
 
 export default function App() {
+  const scale = useSharedValue(1);
+  const translateX = useSharedValue(0);
+
+  const handleScaleAnimation = (value: number) => {
+    scale.value = withTiming(value,{
+      duration: 700,
+      easing: Easing.elastic(2)      
+    });
+  }
+
+  const handleTranslateXAnimation = () => {
+    scale.value = withTiming(1.5, 
+      { duration: 2000 }, 
+      (finished) => {
+      if (finished) {
+        translateX.value = withSequence(
+          withTiming(150),
+          withTiming(-150),
+          withSpring(0, undefined, (finished) => {
+            if (finished) {
+              scale.value = withSpring(1);
+            }
+          })
+        )
+      }
+    });
+  }
+
+  const handlePressIn = () => handleScaleAnimation(1.5);
+  
+  const handlePressOut = () => handleScaleAnimation(1);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      { scale: scale.value },
+      { translateX: translateX.value }
+    ],
+    backgroundColor: interpolateColor(
+      scale.value, 
+      [1, 1.5], 
+      ['blue', 'red']
+    )
+  }));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View 
+        style={[styles.box, animatedStyle]} 
+      />
+
+      <Pressable
+        style={styles.button}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+      >
+        <Text>Clique Aqui</Text>
+      </Pressable>
+
+      <Button 
+        title='Animar' 
+        onPress={handleTranslateXAnimation}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 32,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    borderRadius: 7,
+    margin: 32
+  },
+  button: {
+    height: 56,
+    width: '100%',
+    backgroundColor: '#CECECE',
+    borderRadius: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 42
+  }
+});
